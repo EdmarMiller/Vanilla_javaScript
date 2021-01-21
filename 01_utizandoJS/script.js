62266569ec0a584eec46eeefd899be520eae162f
EdmarMiller <edmarmiller@hotmail.com>
2021-01-21 09:59:52 -0300

    Pasta utilizando JS

---
+++ 01_utizandoJS/script.js
@@ -0,0 +1,9 @@
+function mostrarAlert(){
+  alert('No Arquivo Externo _ Alert');
+}
+function mostrarConfirm(){
+  confirm('No Arquivo Externo _ Confirm')
+}
+function mostrarPrompt(){
+  prompt('Qual seu nome?')
+}
